83c167920dc20d8c3bc28dd534657fecde27e992
Steve Jones <steve.jones763@gmail.com>
2021-09-20 12:38:11 +0100

    tests for each method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,20 @@
 class BankAccount
+
+  def initialize
+      @balance = 0
+  end
+
   def deposit(amount)
-    return "You deposited £#{amount}"
+    @balance += amount
+    "You deposited £#{amount}"
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+    "You withdrew £#{amount}"
+  end
+
+  def view_balance
+    "Your current balance is £#{@balance}"
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,7 +5,38 @@ describe BankAccount do
   describe 'deposit' do
     it 'adds money to the customers balance' do
       amount = 10
-      expect(BankAccount.new.deposit(amount)).to eq("You deposited £#{amount}")
+      expect(subject.deposit(amount)).to eq("You deposited £#{amount}")
     end
+  end
+
+  describe 'withdraw' do
+    it 'withdraws money from the customers balance' do
+      amount = 10
+      expect(subject.withdraw(amount)).to eq("You withdrew £#{amount}")
+    end
+  end
+
+  describe 'view_balance' do
+    context "balance is £0" do
+      it 'prints the customers current balance' do 
+        expect(subject.view_balance).to eq("Your current balance is £0")
+      end
+    end
+
+    context "balance is £10" do
+      it 'prints the customers current balance' do 
+        subject.deposit(10)
+        expect(subject.view_balance).to eq("Your current balance is £10")
+      end
+    end
+
+    context "balance is £100 after a £10 withdrawal" do
+      it 'prints the customers current balance' do 
+        subject.deposit(110)
+        subject.withdraw(10)
+        expect(subject.view_balance).to eq("Your current balance is £100")
+      end
+    end
+   
   end
 end
